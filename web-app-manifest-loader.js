7246f0b361f6868bab56bbaedb97a4b781cab6f6
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2015-12-16 22:40:35 +1100

    Support multiple icons

--- web-app-manifest-loader.js
+++ web-app-manifest-loader.js
@@ -1,5 +1,33 @@
 var path = require('path');
 var loaderUtils = require('loader-utils');
+var steed = require('steed');
+
+function resolveIconSrc(loaderContext, icon, callback) {
+  var dirname = path.dirname(loaderContext.resourcePath);
+
+  // Resolve the icon filename relative to the manifest file
+  loaderContext.resolve(dirname, icon.src, function(err, filename) {
+    if (err) {
+      return callback(err);
+    }
+
+    // Ensure Webpack knows that the icon is a dependency of the manifest
+    loaderContext.dependency && loaderContext.dependency(filename);
+
+    // Asynchronously pass the image through the loader pipeline
+    loaderContext.loadModule(filename, function(err, source, map, module) {
+      if (err) {
+        return callback(err);
+      }
+
+      // Update the icon src property to match the generated filename
+      // Is it always the first key in the assets object?
+      icon.src = Object.keys(module.assets)[0];
+
+      callback(null, icon);
+    });
+  });
+}
 
 module.exports = function(source) {
   var manifest = JSON.parse(source);
@@ -7,32 +35,17 @@ module.exports = function(source) {
   var loaderContext = this;
   var callback = loaderContext.async();
 
-  var dirname = path.dirname(loaderContext.resourcePath);
-
   if (Array.isArray(manifest.icons)) {
-    // Hard-coded to only resolve the first icon for now
-    loaderContext.resolve(dirname, manifest.icons[0].src, function(err, filename) {
+    steed.map(manifest.icons, resolveIconSrc.bind(null, loaderContext), function(err, icons) {
       if (err) {
-				return callback(err);
-			}
+        return callback(err);
+      }
 
-      // Ensure Webpack knows that the icon is a dependency of the manifest
-      loaderContext.dependency && loaderContext.dependency(filename);
+      manifest.icons = icons;
 
-      // Asynchronously pass the image through the loader pipeline
-      loaderContext.loadModule(filename, function(err, source, map, module) {
-        if (err) {
-          return callback(err);
-        }
-
-        // Update the icon src property to match the generated filename
-        // Is it always the first key in the assets object?
-        manifest.icons[0].src = Object.keys(module.assets)[0];
-
-        callback(null, JSON.stringify(manifest, null, 2));
-      });
+      callback(null, JSON.stringify(manifest, null, 2));
     });
   } else {
-    callback(null, JSON.stringify(manifest));
+    callback(null, JSON.stringify(manifest, null, 2));
   }
 };
